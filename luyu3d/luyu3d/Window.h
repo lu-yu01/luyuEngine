25ca5d18801dbe1fa9654b3940339a39a893638e
lu-yu01 <jia.gao.rv@gmail.com>
2021-08-12 23:02:53 +0800

    add

---
+++ luyu3d/luyu3d/Window.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "luyuWin.h"
+
+class Window
+{
+private:
+	// singleton manages registration / cleanup of window class
+	class WindowClass
+	{
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstace() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator = (const WindowClass&) = delete;
+		static constexpr const char* wndClassName = "luyu Direct3D Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const char* name) noexcept;
+	~Window();
+	Window(const Window& ) = delete;
+	Window& operator = (const Window&) = delete;
+private:
+	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
+
