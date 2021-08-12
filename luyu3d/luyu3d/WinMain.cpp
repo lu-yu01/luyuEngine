528964f5a4288c653ac04b6b9cbe7dd1218a0c60
lu-yu01 <jia.gao.rv@gmail.com>
2021-08-13 00:14:43 +0800

    Update WinMain.cpp

--- luyu3d/luyu3d/WinMain.cpp
+++ luyu3d/luyu3d/WinMain.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 #include "WindowsMessageMap.h"
-
+#include <sstream>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -23,6 +23,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 				SetWindowText(hWnd, "luyu leave");
 			}
 			break;
+		case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowText(hWnd, title.c_str());
+		}
+		break;
+		case WM_LBUTTONDOWN:
+		{
+			const POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowText(hWnd, oss.str().c_str());
+		}
+		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
