106ede3832ed607a2eaa85df49e728c4f2754e1f
lu-yu01 <jia.gao.rv@gmail.com>
2021-10-05 23:25:45 +0800

    增加三角形着色器 颜色

--- luyu3d/luyu3d/Graphics.cpp
+++ luyu3d/luyu3d/Graphics.cpp
@@ -108,16 +108,17 @@ void Graphics::DrawTestTriangle()
 	{
 		float x;
 		float y;
+		float r;
+		float g;
+		float b;
 	};
 
 	//create vertex buffer (1  2d  triangle at center of screen)
 	const Vertex vertices[] =
 	{
-		{0.0f, 0.5f },
-		{ 0.5f, -0.5f },
-		{ -0.5f, -0.5f },
-		{0.0f, 0.5f },
-
+		{0.0f, 0.5f,1.0f, 0.0f, 0.0f },
+		{0.5f, -0.5f, 0.0f, 1.0f,0.0f },
+		{-0.5f, -0.5f, 0.0f, 0.0f, 1.0f },
 	};
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 	D3D11_BUFFER_DESC bd = {};
@@ -162,6 +163,8 @@ void Graphics::DrawTestTriangle()
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
 		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0 ,0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+        {"Color", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0 ,8u, D3D11_INPUT_PER_VERTEX_DATA, 0},
+
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
@@ -178,7 +181,7 @@ void Graphics::DrawTestTriangle()
 
 	
 	// Set primitive topology to triangle list(group of 3 vertices)
-	pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_LINESTRIP);
+	pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	//configure viewport
 	D3D11_VIEWPORT vp;
